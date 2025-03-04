cbf8f4959ba00b08b3be34eeca126a223247f708
leslenaaa <vh3081001@gmail.com>
2025-03-03 21:04:23 -0600

    python

--- logica.py
+++ logica.py
@@ -1,7 +1,7 @@
 #un cajero automatico
 Usuario = list()
 
-Usuario.append("theHacker2244")
+Usuario.append("leslena")
 Usuario.append("1234")
 Usuario.append(1000)
 
@@ -22,17 +22,18 @@ acciones.append("pago de resibo")
 acciones.append("Retirar")
 
 
-"Usuario2".append("Stacy Recinos")
-"Usuario2".append("12345")
-"Usuario2".append(1500)
+Usuario2 = list()
+Usuario2.append("leslenaa")
+Usuario2.append("12345")
+Usuario2.append(1500)
 
-def registrar()
-    UsuarioRegistrado= list()
-    usuarioc =input("Ingrese su nombre: ")
-    UsuarioRegistrado.append("Ingrese su password")
+def registrar():
+    UsuarioRegistrado = list()
+    usuarioc = input("Ingrese su nombre: ")
+    UsuarioRegistrado.append(input("Ingrese su password"))
 
-    if "usuarioc" ==[0] and UsuarioRegistrado[0] == Usuario[1]
-       print("Bienvenido al cajero automatico")
+    if usuarioc == Usuario[0] and UsuarioRegistrado[0] == Usuario[1]:
+        print("Bienvenido al cajero automatico")
     else:
         print("Usuario o contraseña incorrecta")
         return registrar()
